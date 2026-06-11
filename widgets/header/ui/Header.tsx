672a071dfce77eb666563b830fd4c2ee695b3574
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-06-12 00:26:40 +0300

    feat: added Header widget

---
+++ widgets/header/ui/Header.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import {HeaderNav} from "@/widgets/header/ui/HeaderNav";
+
+export function Header() {
+    return (
+        <header className="fixed top-0 left-0 w-full h-24 px-4 flex items-center justify-between bg-white shadow-lg">
+            <Image src="logo.svg" alt="company logo" className="w-28" width={256} height={256} />
+            <HeaderNav />
+        </header>
+    )
+}
---
+++ widgets/header/ui/HeaderNav.tsx
@@ -0,0 +1,12 @@
+import {IconLink} from "@/shared/links/IconLink";
+
+export function HeaderNav() {
+    return (
+        <section className="flex gap-8">
+            <IconLink href="/" iconSrc="icons/products.svg">Каталог</IconLink>
+            <IconLink href="/" iconSrc="icons/products.svg">Каталог</IconLink>
+            <IconLink href="/" iconSrc="icons/products.svg">Каталог</IconLink>
+
+        </section>
+    )
+}
