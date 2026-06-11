584e41d5574678af73080cfefbf5a4c15f474c9b
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-06-12 00:27:06 +0300

    feat: added IconLink component

--- shared/links/IconLink.tsx
+++ shared/links/IconLink.tsx
@@ -13,11 +13,15 @@ type Props = {
 
 export function IconLink({href, iconSrc, children, className}: Props) {
     return (
-        <Link href={href} className={cn("text-secondary text-3xl font-medium group/link", className)}>
-            <Image src={iconSrc} alt={"nav icon"} className="w-8 h-8"/>
-            <div className="relative flex flex-col items-center gap-1">
+        <Link href={href} className={
+            cn(
+                "flex gap-1 text-secondary hover:text-primary text-xl font-medium group/link transition-colors duration-300",
+                className)
+        }>
+            <Image src={iconSrc} alt={"nav icon"} className="w-7 h-7" width={64} height={64} />
+            <div className="relative flex flex-col items-center justify-center gap-0.5">
                 {children}
-                <span className="group-hover/link:w-full h-0.5 rounded-full bg-primar"/>
+                <span className="group-hover/link:w-full  w-0 h-0.5 rounded-full bg-primary transition-all duration-300"/>
             </div>
 
         </Link>
