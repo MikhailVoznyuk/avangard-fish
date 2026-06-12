372a66be4fe5a3e11f22f108203b4b7498da4217
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-06-12 23:20:10 +0300

    refactor

--- shared/links/IconLink.tsx
+++ shared/links/IconLink.tsx
@@ -15,13 +15,13 @@ export function IconLink({href, iconSrc, children, className}: Props) {
     return (
         <Link href={href} className={
             cn(
-                "flex gap-1 text-secondary hover:text-primary text-xl font-medium group/link transition-colors duration-300",
+                "flex gap-1 text-secondary text-xl font-medium group/navLink",
                 className)
         }>
             <Image src={iconSrc} alt={"nav icon"} className="w-7 h-7" width={64} height={64} />
-            <div className="relative flex flex-col items-center justify-center gap-0.5">
+            <div className="relative group-hover/navLink:-translate-y-0.5  flex flex-col items-center justify-center gap-[1px] transition-transform duration-300">
                 {children}
-                <span className="group-hover/link:w-full  w-0 h-0.5 rounded-full bg-primary transition-all duration-300"/>
+                <span className="group-hover/navLink:w-full w-0 h-0.5 rounded-full bg-primary transition-all duration-300"/>
             </div>
 
         </Link>
