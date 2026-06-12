8de8c493897e88298e07fac4d3783af4cce71c9f
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-06-12 23:18:59 +0300

    refactor

--- widgets/Header/ui/Header.tsx
+++ widgets/Header/ui/Header.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import {HeaderNav} from "@/widgets/header/ui/HeaderNav";
+import {HeaderNav} from "@/widgets/Header/ui/HeaderNav";
 
 export function Header() {
     return (
-        <header className="fixed top-0 left-0 w-full h-24 px-4 flex items-center justify-between bg-white shadow-lg">
+        <header className="fixed z-20 top-0 left-0 w-full h-24 px-4 flex items-center justify-between bg-white shadow-lg">
             <Image src="logo.svg" alt="company logo" className="w-28" width={256} height={256} />
             <HeaderNav />
         </header>
---
+++ widgets/Header/ui/HeaderNav.tsx
@@ -0,0 +1,12 @@
+import {IconLink} from "@/shared/links/IconLink";
+
+export function HeaderNav() {
+    return (
+        <section className="flex gap-8">
+            <IconLink href="/" iconSrc="icons/products_primary.svg">Каталог</IconLink>
+            <IconLink href="/" iconSrc="icons/products_primary.svg">О нас</IconLink>
+            <IconLink href="/" iconSrc="icons/products_primary.svg">Контакты</IconLink>
+
+        </section>
+    )
+}
