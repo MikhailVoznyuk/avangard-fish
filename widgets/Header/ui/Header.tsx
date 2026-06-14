7ef42c12b78e35446eb380080a07f61a4a32664c
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-06-14 22:35:52 +0300

    feat: added empty space under fixed header

--- widgets/Header/ui/Header.tsx
+++ widgets/Header/ui/Header.tsx
@@ -3,9 +3,12 @@ import {HeaderNav} from "@/widgets/Header/ui/HeaderNav";
 
 export function Header() {
     return (
-        <header className="fixed z-20 top-0 left-0 w-full h-24 px-4 flex items-center justify-between bg-white shadow-lg">
-            <Image src="logo.svg" alt="company logo" className="w-28" width={256} height={256} />
-            <HeaderNav />
-        </header>
+        <>
+            <header className="fixed z-20 top-0 left-0 w-full h-24 px-4 flex items-center justify-between bg-white shadow-lg">
+                <Image src="logo.svg" alt="company logo" className="w-28" width={256} height={256} />
+                <HeaderNav />
+            </header>
+            <div className='h-24' />
+        </>
     )
 }
