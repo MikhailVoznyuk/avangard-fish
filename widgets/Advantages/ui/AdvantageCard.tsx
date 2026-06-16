58153b9a2abe6dd62b1273048be6186d8b21c3e4
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-06-16 10:16:10 +0300

    feat: finished Advantages section

--- widgets/Advantages/ui/AdvantageCard.tsx
+++ widgets/Advantages/ui/AdvantageCard.tsx
@@ -12,7 +12,7 @@ interface Props {
 export function AdvantageCard({ title, description, image, inverse}: Props) {
     return (
         <Reveal>
-            <div className="flex w-[640px] h-[260px] shadow-[0_0_10px_rgba(0,0,0,0.25)] rounded-2xl bg-white overflow-hidden">
+            <div className="flex w-[640px] h-[254px] shadow-[0_0_10px_rgba(0,0,0,0.25)] rounded-2xl bg-white overflow-hidden">
                 <div className="relative w-2/5 h-full">
                     <Image src={image}
                            alt="advantage side photo"
@@ -23,11 +23,11 @@ export function AdvantageCard({ title, description, image, inverse}: Props) {
                     <div className="absolute top-0 right-0 w-1/5 h-full bg-linear-to-l from-white to-transparent" />
                 </div>
                 <div className="relative w-3/5" >
-                    <div className="absolute z-0 -bottom-[300px] -right-[300px] w-[600px] h-[600px] bg-radial from-secondary/60  to-transparent to-60%" />
-                    <div className="relative z-10 flex flex-col gap-3 p-4">
-                        <Header as={"h3"} variant="secondary" size="lg">{title}</Header>
+                    <div className="absolute z-0 -bottom-[240px] -right-[240px] w-[480px] h-[480px] bg-radial from-secondary  to-transparent to-60%" />
+                    <div className="relative z-10 flex flex-col gap-2 p-4">
+                        <Header as={"h3"} variant="secondary" size="xl">{title}</Header>
                         <span className="w-1/3 h-1 bg-secondary rounded-full" />
-                        <p className="text-regular">{description}</p>
+                        <p className="text-lg text-regular font-normal">{description}</p>
                     </div>
 
                 </div>
--- widgets/Advantages/ui/Advantages.tsx
+++ widgets/Advantages/ui/Advantages.tsx
@@ -1,4 +1,5 @@
 import {AdvantageCard} from "@/widgets/Advantages/ui/AdvantageCard";
+import { Header } from "@/shared/ui/text/Header";
 
 type Advantage = {
     title: string;
@@ -12,27 +13,31 @@ const ADVANTAGES_LIST: Advantage[] = [
 
 export function Advantages() {
     return (
-        <section className="flex justify-center w-full gap-6 flex-wrap">
-            <AdvantageCard
-                title={"Lorem ipsum dolor sit amet"}
-                description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "}
-                image={"/images/hero/fish.png"}
-            />
-            <AdvantageCard
-                title={"Lorem ipsum dolor sit amet"}
-                description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "}
-                image={"/images/hero/fish.png"}
-            />
-            <AdvantageCard
-                title={"Lorem ipsum dolor sit amet"}
-                description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "}
-                image={"/images/hero/fish.png"}
-            />
-            <AdvantageCard
-                title={"Lorem ipsum dolor sit amet"}
-                description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "}
-                image={"/images/hero/fish.png"}
-            />
+        <section className="flex flex-col items-center w-full gap-10">
+            <Header as={"h2"} variant="primary" size="xl" animated>Наши преимущества</Header>
+            <div className="flex justify-center gap-6 flex-wrap">
+                <AdvantageCard
+                    title={"Lorem ipsum dolor sit amet"}
+                    description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "}
+                    image={"/images/hero/fish.png"}
+                />
+                <AdvantageCard
+                    title={"Lorem ipsum dolor sit amet"}
+                    description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "}
+                    image={"/images/hero/fish.png"}
+                />
+                <AdvantageCard
+                    title={"Lorem ipsum dolor sit amet"}
+                    description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "}
+                    image={"/images/hero/fish.png"}
+                />
+                <AdvantageCard
+                    title={"Lorem ipsum dolor sit amet"}
+                    description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "}
+                    image={"/images/hero/fish.png"}
+                />
+            </div>
+
         </section>
     )
 }
