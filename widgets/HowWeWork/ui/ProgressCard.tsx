15b05fd1d6ae051bce75449d1ee185ff4eb46800
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-06-19 23:42:12 +0300

    feat: added card boilerplate

--- widgets/HowWeWork/ui/ProgressCard.tsx
+++ widgets/HowWeWork/ui/ProgressCard.tsx
@@ -1,12 +1,19 @@
 import {type ReactNode} from "react";
+import {Header} from "@/shared/ui/text/Header";
+import {type MotionValue} from "framer-motion";
+import {type ProgressItem} from "@/widgets/HowWeWork/ui/ProgressBlock";
 
 export type Props = {
-    title: string;
-    content: string;
+    item: ProgressItem;
+    progressNeeded: number;
+    currentProgress: MotionValue;
 }
 
-export function TimelineItem({title, content} : Props) {
+export function ProgressCard ({item, progressNeeded, currentProgress} : Props) {
     return (
-        <article className="flex flex-col items-center"></article>
+        <article className="flex flex-col items-center">
+            <Header as="h3" variant="secondary">{item.title}</Header>
+            <p className="text-base text-regular text-center">{item.content}</p>
+        </article>
     )
 }
