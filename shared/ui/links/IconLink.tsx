793e67ce3087ec5662b005f4101a0ed849197b0e
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-06-21 11:27:24 +0300

    feat: added uppercase prop

--- shared/ui/links/IconLink.tsx
+++ shared/ui/links/IconLink.tsx
@@ -9,17 +9,19 @@ type Props = {
     iconSrc: string;
     children: ReactNode;
     className?: ClassValue;
+    uppercase?: boolean;
 }
 
-export function IconLink({href, iconSrc, children, className}: Props) {
+export function IconLink({href, iconSrc, children, className, uppercase}: Props) {
     return (
         <Link href={href} className={
             cn(
                 "flex gap-1 text-secondary text-xl font-medium group/navLink",
                 className)
         }>
-            <Image src={iconSrc} alt={"nav icon"} className="w-7 h-7" width={64} height={64} />
-            <div className="relative group-hover/navLink:-translate-y-0.5  flex flex-col items-center justify-center gap-[1px] transition-transform duration-300">
+            <Image src={iconSrc} alt={"nav icon"} className="size-6" width={64} height={64} />
+            <div
+                className={`${uppercase ? "uppercase text-lg" : ""} relative group-hover/navLink:-translate-y-1  flex flex-col items-center justify-center gap-[1px] font-medium transition-transform duration-300`}>
                 {children}
                 <span className="group-hover/navLink:w-full w-0 h-0.5 rounded-full bg-primary transition-all duration-300"/>
             </div>
--- widgets/Header/ui/HeaderNav.tsx
+++ widgets/Header/ui/HeaderNav.tsx
@@ -3,9 +3,9 @@ import {IconLink} from "@/shared/ui/links/IconLink";
 export function HeaderNav() {
     return (
         <section className="flex gap-8">
-            <IconLink href="/" iconSrc="icons/products_primary.svg">Каталог</IconLink>
-            <IconLink href="/" iconSrc="icons/products_primary.svg">О нас</IconLink>
-            <IconLink href="/" iconSrc="icons/products_primary.svg">Контакты</IconLink>
+            <IconLink href="/" iconSrc="icons/products_primary.svg" uppercase>Каталог</IconLink>
+            <IconLink href="/" iconSrc="icons/products_primary.svg" uppercase>О нас</IconLink>
+            <IconLink href="/" iconSrc="icons/products_primary.svg" uppercase>Контакты</IconLink>
 
         </section>
     )
