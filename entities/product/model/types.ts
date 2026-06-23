f5fb730c336803c6d05d4db07cc2fa02c11d1c2f
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-06-23 10:15:12 +0300

    feat: updated types of product

--- entities/product/model/types.ts
+++ entities/product/model/types.ts
@@ -1,13 +1,15 @@
-type Availability = "В наличии" | "Под заказ" | "Нет в продаже"
-type Category = "Филе" | "Консервы" | "Котлеты" | "Без обработки"
+type Availability = "В наличии" | "Под заказ" | "Нет в продаже";
+type Category = "Филе" | "Консервы" | "Котлеты" | "Без обработки";
+type PriceUnit = "₽/кг" | "₽/шт" | "₽/уп";
+
 export type Product = {
     id: string;
     title: string;
     slug: string;
     description: string;
     price: number;
-    priceUnit: string;
-    tags: string; // через точку с запятой
+    priceUnit: PriceUnit;
+    tags: string[] // через точку с запятой в CRM
     imageUrl: string;
     category: Category;
     availability: Availability;
