88472a065f594ca5c7ae4930303cd1dff0ebeb32
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-06-29 22:56:52 +0300

    fix: card layout with dynamic description

--- entities/product/ui/ProductCard.tsx
+++ entities/product/ui/ProductCard.tsx
@@ -11,8 +11,8 @@ import { Button } from "@/shared/ui/buttons/Button";
 
 export function ProductCard(props: ProductItem) {
     return (
-        <article className="flex flex-col bg-white border border-black/10 rounded-2xl  shadow-lg overflow-hidden w-xs h-[440px] gap-4">
-            <div className="relative w-full h-[200px]">
+        <article className="flex flex-col bg-white border border-black/10 rounded-2xl  shadow-lg overflow-hidden w-xs h-110 gap-4">
+            <div className="relative w-full h-50">
                 <Image
                     src={props.imageUrl}
                     width={320}
@@ -20,18 +20,20 @@ export function ProductCard(props: ProductItem) {
                     alt="product image"
                     className='w-full h-full'
                 />
-                <div className="absolute bottom-0 w-full h-[200px] bg-linear-to-t from-white to-transparent" />
+                <div className="absolute bottom-0 w-full h-50 bg-linear-to-t from-white to-transparent" />
                 <AvailabilityLabel
                     availability={props.availability}
                     className="absolute top-4 right-4"
                 />
             </div>
-            <div className="p-4 flex flex-col gap-3">
-                <TextHeader as="h3" size="md" animated={false}>{props.title}</TextHeader>
-                <div className="flex gap-2">
-                    {props.tags.map((tag) => <Label key={tag} size="sm">{tag}</Label>)}
+            <div className="p-4 flex flex-col justify-between grow">
+                <div className="flex flex-col gap-3">
+                    <TextHeader as="h3" size="md" animated={false}>{props.title}</TextHeader>
+                    <div className="flex gap-2">
+                        {props.tags.map((tag) => <Label key={tag} size="sm">{tag}</Label>)}
+                    </div>
+                    <Text size="md">{props.description}</Text>
                 </div>
-                <Text size="md">{props.description}</Text>
                 <div className="flex justify-between items-center">
                     <TextHeader as="h5" size="lg" className="text-primary" animated={false}>
                         {`${props.price} ${PRODUCT_PRICE_UNIT_LABEL[props.priceUnit]}`}
