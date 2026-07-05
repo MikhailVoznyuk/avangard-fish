e540b81ab93ed377af7961076d9dec895948a3dc
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-07-05 16:49:27 +0300

    style: changed width to 280px

--- widgets/HowWeWork/ui/ProgressCard.tsx
+++ widgets/HowWeWork/ui/ProgressCard.tsx
@@ -30,7 +30,7 @@ export function ProgressCard ({item, progressNeeded, currentProgress} : Props) {
     console.log(progressNeeded, currentProgress)
     return (
         <motion.article
-            className="flex flex-col items-center p-4 bg-white border-regular/20 border-1 rounded-2xl max-w-xs shadow-lg"
+            className="flex flex-col items-center w-[280px] p-4 bg-white border-regular/20 border rounded-2xl max-w-xs shadow-lg"
             style={{opacity, y, scale}}
         >
             <Header as="h3" variant="secondary" centered>{item.title}</Header>
