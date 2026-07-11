21e4c42ef653d3caef989be5cf729f3824525cab
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-07-11 12:07:56 +0300

    feat: added button link ui component

---
+++ shared/ui/links/ButtonLink.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import type { ReactNode } from "react";
+import type { ClassValue } from "clsx";
+import { cn } from "@/shared/utils/cn";
+
+type Props = {
+    href: string;
+    variant?: 'primary' | 'secondary';
+    children: ReactNode;
+    uppercase?: boolean;
+    className?: ClassValue;
+}
+
+export function ButtonLink({href, variant, uppercase, className, children}: Props) {
+    const base: ClassValue = "flex items-center justify-center rounded-full text-xl font-medium cursor-pointer text-white transition-colors duration-300";
+    const optional: ClassValue = variant === "primary" ? "bg-primary uppercase px-3 py-2 hover:bg-secondary" : "bg-secondary px-2 py-1 hover:bg-primary";
+
+    return (
+        <Link href={href} className={cn(base, optional, className, uppercase ? "uppercase" : "")}>{children}</Link>
+    )
+}
