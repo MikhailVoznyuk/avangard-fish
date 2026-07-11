d05473af2d2994bcbcc1d5aa13522304cbf2e303
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-07-11 12:08:38 +0300

    style: added color transition

--- shared/ui/buttons/Button.tsx
+++ shared/ui/buttons/Button.tsx
@@ -3,19 +3,20 @@ import {type ClassValue} from "clsx";
 import {cn} from "@/shared/utils/cn";
 
 type Props = {
+    type?: "button" | "submit";
     handler: () => void;
     variant?: "primary" | "secondary";
     children: ReactNode;
-    className?: string;
+    className?: ClassValue;
 }
 
-export function Button({handler, variant="secondary", children, className}: Props) {
-    const base: ClassValue = "flex items-center justify-center rounded-full text-xl font-medium cursor-pointer text-white";
-    const optional = variant === "primary" ? "bg-primary uppercase px-3 py-2" : "bg-secondary px-2 py-1";
+export function Button({type="button", handler, variant="secondary", children, className}: Props) {
+    const base: ClassValue = "flex items-center justify-center rounded-full text-xl font-medium cursor-pointer text-white transition-colors duration-300";
+    const optional: ClassValue = variant === "primary" ? "bg-primary uppercase px-3 py-2 hover:bg-secondary" : "bg-secondary px-2 py-1 hover:bg-primary";
 
     return (
         <button
-            type="button"
+            type={type}
             onClick={handler}
             className={cn("", base, optional, className)}
         >
