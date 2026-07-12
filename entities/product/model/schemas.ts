900aaf9984ddbd9971c5a5ff3f259ed5f4f00a33
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-07-12 23:57:52 +0300

    feat: added description to fields

--- entities/product/model/schemas.ts
+++ entities/product/model/schemas.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 export const productFormSchema = z.object({
-    title: z.string().min(4),
-    slug: z.string().min(4),
-    description: z.string().min(10),
-    price: z.coerce.number().int().positive(),
+    title: z.string().min(4, "Название слишком короткое"),
+    slug: z.string().min(4, "Ссылка слишком короткая"),
+    description: z.string().min(10, "Описание слишком короткое"),
+    price: z.coerce.number().int("Цена должна быть целым числом").positive("Цена должна быть больше нуля"),
     priceUnit: z.enum(["PER_KG", "PER_ITEM", "PER_PACKAGE"]),
     tags: z.string(),
     imageUrl: z.string().min(4),
