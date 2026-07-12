80a69d74605a0b3095f7f458418bcfae02711fb8
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-07-12 23:57:08 +0300

    feat: added tags parse lib util

---
+++ entities/product/model/lib.ts
@@ -0,0 +1,10 @@
+export function parseProductTags(value: string) {
+    return Array.from(
+        new Set(
+            value
+                .split(",")
+                .map((item) => item.trim())
+                .filter(Boolean)
+        )
+    );
+}
