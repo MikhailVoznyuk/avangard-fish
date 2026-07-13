f940bb4e15c9d83f6e5fa6124e7b8be1aca9f8ca
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-07-13 09:51:11 +0300

    feat: added AdvantageCardRadial component

--- widgets/Advantages/ui/Advantages.tsx
+++ widgets/Advantages/ui/Advantages.tsx
@@ -1,4 +1,5 @@
-import {AdvantageCard} from "@/widgets/Advantages/ui/AdvantageCard";
+import { AdvantageCard } from "@/widgets/Advantages/ui/AdvantageCard";
+import { AdvantageCardRadial } from "@/widgets/Advantages/ui/AdvantageCardRadial";
 import { SectionContainer } from "@/shared/ui/containers/SectionContainer";
 
 type Advantage = {
@@ -15,20 +16,27 @@ export function Advantages() {
     return (
         <SectionContainer title="Наши преимущества">
             <div className="flex justify-center gap-6 flex-wrap">
-                <AdvantageCard
-                    title={"Lorem ipsum dolor sit amet"}
-                    description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "}
-                    image={"/images/hero/fish.png"}
+                <AdvantageCardRadial
+                    title={"Надежность."}
+                    description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}
+                    imageUrl={"/images/hero/fish.png"}
+                />
+                <AdvantageCardRadial
+                    title={"Качество."}
+                    description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}
+                    imageUrl={"/images/hero/fish.png"}
+                    inverse
                 />
                 <AdvantageCard
-                    title={"Lorem ipsum dolor sit amet"}
-                    description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "}
+                    title={"Быстрые сроки."}
+                    description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "}
                     image={"/images/hero/fish.png"}
                 />
-                <AdvantageCard
-                    title={"Lorem ipsum dolor sit amet"}
-                    description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "}
-                    image={"/images/hero/fish.png"}
+                <AdvantageCardRadial
+                    title={"Быстрые сроки."}
+                    description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}
+                    imageUrl={"/images/hero/fish.png"}
+                    inverse
                 />
                 <AdvantageCard
                     title={"Lorem ipsum dolor sit amet"}
