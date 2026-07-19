73ddd9cfb7773d5ee2bd44a312650f26ffa63779
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-07-19 19:56:16 +0300

    feat: added login page

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/widgets/LoginForm";
+
+export default function AdminPage() {
+    return (
+        <main
+            className={"min-h-screen flex justify-center items-center"}
+        >
+            <LoginForm />
+        </main>
+    )
+}
