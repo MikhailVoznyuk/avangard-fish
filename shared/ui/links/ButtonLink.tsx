57773a13f3b51bf6810f6f177f027445d1b88dc2
MikhailVoznyuk <mikhailv.commerce@gmail.com>
2026-07-19 19:59:13 +0300

    fix: uppercase bug

--- shared/ui/links/ButtonLink.tsx
+++ shared/ui/links/ButtonLink.tsx
@@ -13,9 +13,9 @@ type Props = {
 
 export function ButtonLink({href, variant, uppercase, className, children}: Props) {
     const base: ClassValue = "flex items-center justify-center rounded-full text-xl font-medium cursor-pointer text-white transition-colors duration-300";
-    const optional: ClassValue = variant === "primary" ? "bg-primary uppercase px-3 py-2 hover:bg-secondary" : "bg-secondary px-2 py-1 hover:bg-primary";
+    const optional: ClassValue = variant === "primary" ? "bg-primary px-3 py-2 hover:bg-secondary" : "bg-secondary px-2 py-1 hover:bg-primary";
 
     return (
-        <Link href={href} className={cn(base, optional, className, uppercase ? "uppercase" : "")}>{children}</Link>
+        <Link href={href} className={cn(base, optional, className, (uppercase || (variant === "primary" && uppercase !== false)) ? "uppercase" : "")}>{children}</Link>
     )
 }
